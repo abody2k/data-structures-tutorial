28426ffb5a553fe3ee9cbf1fe529fa0370e52655
abody2k <Abdulrahman.7.2k@gmail.com>
2023-09-20 12:43:05 +0300

    Added HashMap

--- src/main/java/tutorial/Tutorial.java
+++ src/main/java/tutorial/Tutorial.java
@@ -2,13 +2,16 @@ package tutorial;
 
 import java.util.List;
 import java.util.ArrayList;
-
+import java.util.HashMap;
+import java.util.Vector;
 public class Tutorial {
 
 	public static void main(String[] args) {
-		
+		Vector x= new Vector();
+		x.add(22);
 		List<String> animals = new ArrayList<>();
-
+		HashMap<String,String> names = new HashMap<>();
+		names.put("Animal 1", "Created by Me yohohoho");
 		System.out.println("Hello there");
 
 	}
