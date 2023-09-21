9f88df63c0433e7eadab1c8adfbb3206950aea59
abody2k <Abdulrahman.7.2k@gmail.com>
2023-09-21 09:22:06 +0300

    Finished all types of Deques

--- src/main/java/tutorial/Tutorial.java
+++ src/main/java/tutorial/Tutorial.java
@@ -1,10 +1,8 @@
 package tutorial;
 
 
-import java.util.Queue;
-import java.util.PriorityQueue;
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.LinkedList;
 
 
@@ -12,27 +10,12 @@ public class Tutorial {
 
 	public static void main(String[] args) {
 
-		Queue<Integer> a = new PriorityQueue<>();
-		Queue<Double> b = new LinkedList<>();
-		a.addAll(Arrays.asList(3,1,2,4,66,-200));
-		b.addAll(Arrays.asList(1.0,2.0,3.0,4.0,4.4,-2.00));
-		a.forEach(System.out::println);
-		b.forEach(System.out::println);
+		Deque<Integer> a = new ArrayDeque<>();
+		Deque<Double> b = new LinkedList<>();
 		
-		for(Iterator<?> it = a.iterator();it.hasNext();) {
-			
-			
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println("Just finished sleeping");
-			it.next();
+		
+		
 
-			it.remove();
-		}
 
 	
 		
